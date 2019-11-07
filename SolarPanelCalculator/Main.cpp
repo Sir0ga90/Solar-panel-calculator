a5a988f4adfaaadd6c3e23f21e19fd68d5cb8214
Sir0ga90 <1sergey99chursin0@gmail.com>
2019-11-07 12:46:38 +0200

    Inital commit.

---
+++ SolarPanelCalculator/Main.cpp
@@ -0,0 +1,26 @@
+//UI======================================================================
+//show brief manual
+
+//prompt to enter needed amount of energy
+//prompt ot enter avg sun hours: /summer/, winter only
+
+/*
+ *show result with number of needed: 
+ *  batteries, 
+ *  solar panels,
+ *  needed area on the roof (not only roof is possible)
+*/
+
+//CALCULATIONS============================================================
+//calculate number of batteries, 26 Ah
+//calculate number of solar panels, 250W
+//calculate needed area
+
+
+
+
+int main() {
+    
+
+    return 0;
+}
